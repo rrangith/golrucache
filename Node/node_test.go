a25be5314af075393eb2e485475a73a8ada82115
rrangith <rrangith@edu.uwaterloo.ca>
2020-04-22 00:22:14 -0400

    Tests for doubly node

---
+++ Node/node_test.go
@@ -0,0 +1,156 @@
+package node
+
+import "testing"
+
+func TestMake(t *testing.T) {
+	n := MakeNode(1, nil, nil)
+
+	if (n.val != 1) {
+		t.Errorf("Val was incorrect, got: %d, want: %d.", n.val, 1)
+	}
+
+	if (n.next != nil) {
+		t.Errorf("Next should be nil, but it is not")
+	}
+
+	if (n.prev != nil) {
+		t.Errorf("Prev should be nil, but it is not")
+	}
+}
+
+func TestMakeString(t *testing.T) {
+	n := MakeNode("hi", nil, nil)
+
+	if (n.val != "hi") {
+		t.Errorf("Val was incorrect, got: %s, want: %s.", n.val, "hi")
+	}
+}
+
+func TestGetVal(t *testing.T) {
+	n := MakeNode("hi", nil, nil)
+
+	if (n.GetVal() != "hi") {
+		t.Errorf("Val was incorrect, got: %s, want: %s.", n.val, "hi")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	n1 := MakeNode("hi", nil, nil)
+	n2 := MakeNode(1, nil, nil)
+	n3 := MakeNode(3, nil, nil)
+
+	n1.SetNext(n2)
+	n2.SetNext(n3)
+
+	n2.RemoveNode()
+
+	if (n1.next != n3) {
+		t.Errorf("n1 next should point to n3")
+	}
+
+	if (n3.prev != n1) {
+		t.Errorf("n3 prev should point to n1")
+	}
+
+	if (n2.next != nil) {
+		t.Errorf("n2 next should point to nil")
+	}
+
+	if (n2.prev != nil) {
+		t.Errorf("n2 prev should point to nil")
+	}
+}
+
+func TestSetNext(t *testing.T) {
+	n1 := MakeNode("hi", nil, nil)
+	n2 := MakeNode(1, nil, nil)
+
+	n1.SetNext(n2)
+	n2.SetNextVal(3)
+
+	n3 := n2.next
+
+	if (n1.next != n2) {
+		t.Errorf("n1 next should point to n2")
+	}
+
+	if (n1.prev != nil) {
+		t.Errorf("n1 prev should point to nil")
+	}
+
+	if (n2.next != n3) {
+		t.Errorf("n2 next should point to n3")
+	}
+
+	if (n2.prev != n1) {
+		t.Errorf("n2 prev should point to n1")
+	}
+
+	if (n3.next != nil) {
+		t.Errorf("n3 next should point to n3")
+	}
+
+	if (n3.prev != n2) {
+		t.Errorf("n3 prev should point to n2")
+	}
+}
+
+func TestGetNext(t *testing.T) {
+	n1 := MakeNode("hi", nil, nil)
+	n2 := MakeNode(1, nil, nil)
+
+	n1.SetNext(n2)
+
+	n1Next := n1.GetNext()
+
+	if (n1Next != n2) {
+		t.Errorf("n1 next should point to n2")
+	}
+}
+
+func TestSetPrev(t *testing.T) {
+	n3 := MakeNode("hi", nil, nil)
+	n2 := MakeNode(1, nil, nil)
+
+	n3.SetPrev(n2)
+	n2.SetPrevVal(3)
+
+	n1 := n2.prev
+
+	if (n1.next != n2) {
+		t.Errorf("n1 next should point to n2")
+	}
+
+	if (n1.prev != nil) {
+		t.Errorf("n1 prev should point to nil")
+	}
+
+	if (n2.next != n3) {
+		t.Errorf("n2 next should point to n3")
+	}
+
+	if (n2.prev != n1) {
+		t.Errorf("n2 prev should point to n1")
+	}
+
+	if (n3.next != nil) {
+		t.Errorf("n3 next should point to n3")
+	}
+
+	if (n3.prev != n2) {
+		t.Errorf("n3 prev should point to n2")
+	}
+}
+
+func TestGetPrev(t *testing.T) {
+	n1 := MakeNode("hi", nil, nil)
+	n2 := MakeNode(1, nil, nil)
+
+	n1.SetNext(n2)
+
+	n2Prev := n2.GetPrev()
+
+	if (n2Prev != n1) {
+		t.Errorf("n2 prev should point to n1")
+	}
+}
